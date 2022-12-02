4522dc54d72856382cd3737adb3aa6711e0c25ae
AegisVP <vlad@pysarenko.com>
2022-12-02 05:36:56 +0100

    Static page. prepare for CORS

--- app.js
+++ app.js
@@ -8,6 +8,41 @@ const app = express();
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
+/*
+// Configuring CORS
+const corsOptions = {
+  origin: 'http://example.com',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+// Configuring CORS w/ Dynamic Origin
+const corsOptions = {
+  origin: function (origin, callback) {
+    // db.loadOrigins is an example call to load
+    // a list of origins from a backing database
+    db.loadOrigins(function (error, origins) {
+      callback(error, origins);
+    });
+  },
+};
+
+// Configuring CORS Asynchronously
+var allowlist = ['http://example1.com', 'http://example2.com'];
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptionsDelegate); // callback expects two parameters: error and options
+};
+
+// cors(corsOptions)
+
+// https://expressjs.com/en/resources/middleware/cors.html
+*/
+
 app.use(morgan(formatsLogger));
 app.use(cors());
 app.use(express.json());
