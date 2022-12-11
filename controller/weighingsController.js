0fbedc9fb9dc547bfee81d151be15de9ff9a4d12
AegisVP <vlad@pysarenko.com>
2022-12-11 03:09:41 +0100

    date field to object {year,month,day}

--- controller/weighingsController.js
+++ controller/weighingsController.js
@@ -1,37 +1,55 @@
-const { requestError, allConstants } = require('../utils');
+const { requestError } = require('../utils');
 const { Weighings } = require('../model');
+const { subscriptionsList, driversList, sourcesList, destinationsList, harvestersList, cropsList } = require('../utils').allConstants;
+
+/*
+
+const enteredDate = new Date(weighingRecord.date);
+const [enteredYear, enteredMonth, enteredDay] = [enteredDate.getFullYear(), enteredDate.getMonth(), enteredDate.getDate()];
+
+console.log({ enteredDate, enteredDay, enteredMonth, enteredYear });
+  
+*/
 
 const getWeighings = async (req, res, next) => {
-  // const { subscriptionsList, driversList, sourcesList, destinationsList, harvestersList, cropsList } = allConstants;
-  const { subscriptionsList } = allConstants;
   if (req.user.subscription === subscriptionsList[0]) return next(requestError(401, 'Not authorized', 'NotQualified'));
 
-  // const searchParams = {};
-  // const { source } = req.query;
+  const searchParams = {};
+  const { source, driver, date } = req.query;
+  let { year, month, day } = req.query;
 
+  if (!year || !month || !day) {
+    const parseDate = new Date(date || Date.now());
+    [year, month, day] = [parseDate.getFullYear(), parseDate.getMonth() + 1, parseDate.getDate()];
+  }
+
+  year = parseInt(year);
+  month = parseInt(month);
+  day = parseInt(day);
+
+  searchParams.date = { year, month, day };
+  if (source) searchParams['crop.source'] = source;
+  if (driver) searchParams['auto.driver'] = driver;
+  // if (source) searchParams['crop.source'] = source;
   // if (source) searchParams['crop.source'] = source;
 
-  // const result = await Weighings.find(searchParams);
-  // req.params
-  // req.query
+  console.log({ searchParams });
+  const result = await Weighings.find(searchParams);
 
-  // const {}
-
-  // res.json({ length: result.length, weighings: result });
-  res.json({ message: 'getWeighings ran' });
+  res.json({ length: result.length, weighings: result });
+  // res.json({ message: 'getWeighings ran' });
 };
 
 const addWeighing = async (req, res, next) => {
   const warnings = [];
   const weighingRecord = req.body;
-  const { subscriptionsList, driversList, sourcesList, destinationsList, harvestersList, cropsList } = allConstants;
 
   if (req.user.subscription !== subscriptionsList[1]) return next(requestError(401, 'Not authorized', 'NotQualified'));
 
-  if (driversList.indexOf(weighingRecord.auto.driver) === -1) return next(requestError(400, 'Invalid driver name', 'NoSuchName'));
-  if (cropsList.indexOf(weighingRecord.crop.name) === -1) return next(requestError(400, 'Invalid crop name', 'NoSuchName'));
-  if (sourcesList.indexOf(weighingRecord.crop.source) === -1) return next(requestError(400, 'Invalid crop source', 'NoSuchName'));
-  if (destinationsList.indexOf(weighingRecord.crop.destination) === -1) return next(requestError(400, 'Invalid crop destination', 'NoSuchName'));
+  if (!driversList.includes(weighingRecord.auto.driver)) return next(requestError(400, 'Invalid driver name', 'NoSuchName'));
+  if (!cropsList.includes(weighingRecord.crop.name)) return next(requestError(400, 'Invalid crop name', 'NoSuchName'));
+  if (!sourcesList.includes(weighingRecord.crop.source)) return next(requestError(400, 'Invalid crop source', 'NoSuchName'));
+  if (!destinationsList.includes(weighingRecord.crop.destination)) return next(requestError(400, 'Invalid crop destination', 'NoSuchName'));
 
   const {
     weighing: { brutto, tare, netto },
@@ -51,7 +69,7 @@ const addWeighing = async (req, res, next) => {
     let totalWeight = 0;
 
     for (const { name, weight } of harvesters) {
-      if (harvestersList.indexOf(name) === -1) return next(requestError(400, `Invalid harvester name ${name}`, 'NoSuchName'));
+      if (!harvestersList.includes(name)) return next(requestError(400, `Invalid harvester name ${name}`, 'NoSuchName'));
 
       totalWeight += parseInt(weight);
       harvestersClone.push({ name, weight: nettoForEachHarvester });
@@ -66,6 +84,7 @@ const addWeighing = async (req, res, next) => {
   weighingRecord.createdBy = req.user.email;
 
   console.log(weighingRecord);
+  // const result = weighingRecord;
   const result = await Weighings.create(weighingRecord);
 
   return res.json({ ...(warnings.length && { warnings }), result });
